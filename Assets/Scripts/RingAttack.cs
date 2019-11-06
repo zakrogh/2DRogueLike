c519d05c10ad53abbb78f38929feab255ac10484
Student <example@example.com>
2019-11-06 13:53:20 -0800

    initial attempts to trigger player attack

---
+++ Assets/Scripts/RingAttack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RingAttack : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+      animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Input.GetKeyDown(KeyCode.Space))
+        animator.SetTrigger("ringAttack");
+    }
+    // public void FireAnimation(Vector3 coords)
+    // {
+    //   animator.SetTrigger("ringAttack");
+    // }
+
+}
